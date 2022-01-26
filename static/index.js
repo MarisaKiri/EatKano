f577e448b37512ec68cbbbf5ff560a023267db2f
MarisaKiri <98435008+MarisaKiri@users.noreply.github.com>
2022-01-26 08:35:32 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -348,11 +348,11 @@
             return '倒计时多了' + ((deviation_time / 1000) - 20).toFixed(2) + "s";
         }
         SubmitResults();
-        if (score <= 49) return '试着好好练一下？';
-        if (score <= 99) return 'TCL';
-        if (score <= 149) return 'TQL';
-        if (score <= 199) return '您';
-        return '人？';
+        if (score <= 49) return '还不够塞牙缝呢~';
+        if (score <= 99) return '还不够还不够~';
+        if (score <= 149) return '碎骨有些多呢~';
+        if (score <= 199) return '真是大快朵颐了一番！';
+        return '吃撑啦————';
     }
 
     w.toStr = function(obj) {
